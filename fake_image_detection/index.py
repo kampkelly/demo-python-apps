48019f43953d23799d0afe5e4c8204d01ababfca
Runor Adjekpiyede <kampkellykeys@gmail.com>
2023-06-30 23:12:37 +0100

    add logs

--- fake_image_detection/index.py
+++ fake_image_detection/index.py
@@ -7,18 +7,25 @@ input_size=128
 
 def predict(file_path):
     print('in function', file_path)
+    print('>>>>1')
 
     #Making Single new prediction
     import numpy as np
     from tensorflow.keras.utils import load_img, img_to_array
     test_image = load_img(file_path, target_size = (input_size, input_size))
+    print('>>>>2')
     test_image = img_to_array(test_image)
+    print('>>>>3')
     test_image = test_image /255.
+    print('>>>>4')
     #use this function to change the image from 2dimension(64, 64) to 3dimension(64, 64, 3)
     test_image = np.expand_dims(test_image, axis=0)
+    print('>>>>5')
     #add one more dimension before predicting because the predict method expects a batch
     images = np.vstack([test_image])
+    print('>>>>6')
     result = model.predict(images, batch_size=10)
+    print('>>>>7')
 
     prediction = ''
     if np.argmax(result) == 0:
@@ -38,11 +45,10 @@ def predict(file_path):
     elif np.argmax(result) == 6:
         prediction = 'youtube'
     print(prediction)
+    print('>>>>8')
     
     # remove file from uploads folder
     os.remove(file_path)
-
-
-        
+    print('>>>>9')
 
     return prediction
